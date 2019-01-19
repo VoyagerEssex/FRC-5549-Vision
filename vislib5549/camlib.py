13182f5a1326da05be06b69518afd018b309b087
VoyagerEssex <33985656+VoyagerEssex@users.noreply.github.com>
2019-01-19 11:22:38 -0500

    Added writing feature for camlib.
    Added writing capabilities to camlib in order to write to file.

--- vislib5549/camlib.py
+++ vislib5549/camlib.py
@@ -10,7 +10,7 @@ class CamLib(object):
 
     @staticmethod
     def cv_camera_source(platform):
-        """Scales and image to an exact size.
+        """Gets a camera/video source.
         Args:
             platform: A PlatformType or a custom camera string.
         Returns:
@@ -25,7 +25,7 @@ class CamLib(object):
                 camSrc = cv2.VideoCapture("/dev/video1")
 
             except:
-                raise ("Error, Camera not found [USB_CAM_LINUX]")
+                raise ("Error, Video source not found [USB_CAM_LINUX]")
 
         elif platform is 2:
 
@@ -34,7 +34,7 @@ class CamLib(object):
                 camSrc = cv2.VideoCapture(0)
 
             except:
-                raise ("Error, Camera not found [USB_CAM_WIN]")
+                raise ("Error, Video source not found [USB_CAM_WIN]")
 
         elif platform is 3:
 
@@ -43,7 +43,7 @@ class CamLib(object):
                 camSrc = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)I420 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 
             except:
-                raise ("Error, Camera not found [JETSON_TX2_INT]")
+                raise ("Error, Video source not found [JETSON_TX2_INT]")
 
         else:
 
@@ -52,33 +52,69 @@ class CamLib(object):
                 camSrc = cv2.VideoCapture(platform)
 
             except:
-                raise("Error, Camera not found [Custom Input]")
+                raise("Error, Video source not found [Custom Input]")
 
         return camSrc
 
     @staticmethod
-    def cv_display_capture(videoSrc, width, height, title):
-        """Scales and image to an exact size.
+    def cv_display_capture(videoSrc, width, height, title, **kwargs):
+        """Displays a cv2.VideoCapture to a window.
         Args:
             videoSrc: A VideoCapture object.
             width: Width of video.
             height: Height of video.
             title: Title of window.
+            kwarg - keyExit: Value ID of keyboard key. If this value is True then the default key will be 'q'.
         Returns:
             A video feed from the VideoCapture in a new window.
         """
+        keyExit = kwargs.get('keyExit', False)
+
+        if keyExit is type(bool) and keyExit is True:
+            keyExit = ord('q')
+
         if type(videoSrc) is not cv2.VideoCapture:
             raise ("Error, object provided is not cv2.VideoCapture object")
 
-        windowName = "cv2WindowsDisplay"
+        windowName = "cv2WindowDisplay"
         cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
         cv2.resizeWindow(windowName, width, height)
         cv2.setWindowTitle(windowName, title)
 
-        while True:
+        while videoSrc.isOpened():
 
             if cv2.getWindowProperty(windowName, 0) < 0:
                 break
             ret, frame = videoSrc.read()
             cv2.imshow(windowName, frame)
+            if cv2.waitKey(10) & 0xFF is ord(keyExit) and keyExit is True:
+                break
+
             cv2.waitKey(10)
+
+
+
+    @staticmethod
+    def cv_write_video_stream(videoSrc, width, height, videoName, keyExit):
+        """Displays a cv2.VideoCapture to a window.
+                Args:
+                    videoSrc: A VideoCapture object.
+                    keyExit: Value ID of keyboard key.
+                Returns:
+                    No values, runs stream to file until key is pressed.
+                """
+        fourcc = cv2.VideoWriter_fourcc(*'X264')
+        out = cv2.VideoWriter('%s.avi' % videoName, fourcc, 30, (width, height), True)
+
+        while videoSrc.isOpened():
+            ret, frame = videoSrc.read()
+            if ret is True:
+                out.write(frame)
+                if cv2.waitKey(1) & 0xFF is ord(keyExit):
+                    break
+
+            else:
+                break
+
+        videoSrc.release()
+        out.release()
