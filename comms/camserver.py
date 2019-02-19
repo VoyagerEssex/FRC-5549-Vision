399d1343a4267e436edc7183a518efb87b25d300
VoyagerEssex <33985656+VoyagerEssex@users.noreply.github.com>
2019-02-19 13:48:55 -0800

    Update camserver.py

--- comms/camserver.py
+++ comms/camserver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cscore as cs
+import logging
 
 """
 Class creates a server which streams video to a sink or server. In this case,
@@ -11,10 +12,21 @@ class VideoStream:
     Instances = 0
 
     def __init__(self):
-        self._sink = cs.MjpegServer("DS", 8083)
-        self._source = cs.CvSource(("video%d" % VideoStream.Instances), cs.VideoMode.PixelFormat.kMJPEG)
-        self._sink.setSource(self._source)
-        VideoStream.Instances += 1
+        try:
+            logging.basicConfig(level=logging.DEBUG)
+            self.streamFailure = None
+            self._source = cs.CvSource(("video%d" % VideoStream.Instances), cs.VideoMode.PixelFormat.kMJPEG)
+
+        except Exception:
+            self.streamFailure = True
+
+        else:
+            self.streamFailure = False
+            VideoStream._iterInstances()
 
     def putFrame(self, cv_frame: np.ndarray):
         self._source.putFrame(cv_frame)
+
+    @classmethod
+    def _iterInstances(cls):
+        cls.Instances += 1
